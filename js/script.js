cb93c52de975e6157c83273a7e8aea3b0fd9c339
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-08-06 03:43:16 -0400

    Se agrega loader

--- js/script.js
+++ js/script.js
@@ -1,162 +1,167 @@
-$(document).ready(function () {
-  // typing animation
-  (function ($) {
-    $.fn.writeText = function (content) {
-      let contentArray = content.split(""),
-        current = 0,
-        elem = this;
-      setInterval(function () {
-        if (current < contentArray.length) {
-          elem.text(elem.text() + contentArray[current++]);
-        }
-      }, 80);
-    };
-  })(jQuery);
+	window.addEventListener("load", function () {
+	  const loader = document.querySelector(".loader");
+	  loader.className += " hidden";
+	});
 
-  // input text for typing animation
-  $("#holder").writeText("FRONTEND DEVELOPER JR | REACT");
+	$(document).ready(function () {
+	  // typing animation
+	  (function ($) {
+	    $.fn.writeText = function (content) {
+	      let contentArray = content.split(""),
+	        current = 0,
+	        elem = this;
+	      setInterval(function () {
+	        if (current < contentArray.length) {
+	          elem.text(elem.text() + contentArray[current++]);
+	        }
+	      }, 80);
+	    };
+	  })(jQuery);
 
-  // initialize wow.js
-  new WOW().init();
+	  // input text for typing animation
+	  $("#holder").writeText("FRONTEND DEVELOPER JR | REACT");
 
-  // Push the body and the nav over by 285px over
-  let main = function () {
-    $(".fa-bars").click(function () {
-      $(".nav-screen").animate({
-          right: "0px"
-        },
-        200
-      );
+	  // initialize wow.js
+	  new WOW().init();
 
-      $("body").animate({
-          right: "285px"
-        },
-        200
-      );
-    });
+	  // Push the body and the nav over by 285px over
+	  let main = function () {
+	    $(".fa-bars").click(function () {
+	      $(".nav-screen").animate({
+	          right: "0px"
+	        },
+	        200
+	      );
 
-    // Then push them back */
-    $(".fa-times").click(function () {
-      $(".nav-screen").animate({
-          right: "-285px"
-        },
-        200
-      );
+	      $("body").animate({
+	          right: "285px"
+	        },
+	        200
+	      );
+	    });
 
-      $("body").animate({
-          right: "0px"
-        },
-        200
-      );
-    });
+	    // Then push them back */
+	    $(".fa-times").click(function () {
+	      $(".nav-screen").animate({
+	          right: "-285px"
+	        },
+	        200
+	      );
 
-    $(".nav-links a").click(function () {
-      $(".nav-screen").animate({
-          right: "-285px"
-        },
-        500
-      );
+	      $("body").animate({
+	          right: "0px"
+	        },
+	        200
+	      );
+	    });
 
-      $("body").animate({
-          right: "0px"
-        },
-        500
-      );
-    });
-  };
+	    $(".nav-links a").click(function () {
+	      $(".nav-screen").animate({
+	          right: "-285px"
+	        },
+	        500
+	      );
 
-  $(document).ready(main);
+	      $("body").animate({
+	          right: "0px"
+	        },
+	        500
+	      );
+	    });
+	  };
 
-  // initiate full page scroll
+	  $(document).ready(main);
 
-  $("#fullpage").fullpage({
-    scrollBar: true,
-    responsiveWidth: 200,
-    navigation: true,
-    navigationTooltips: ["home", "aboutme", "skills", "portfolio", "contact", "connect"],
-    anchors: ["home", "aboutme", "skills", "portfolio", "contact", "connect"],
-    menu: "#myMenu",
-    fitToSection: false,
+	  // initiate full page scroll
 
-    afterLoad: function (anchorLink, index) {
-      let loadedSection = $(this);
+	  $("#fullpage").fullpage({
+	    scrollBar: true,
+	    responsiveWidth: 200,
+	    navigation: true,
+	    navigationTooltips: ["home", "aboutme", "skills", "portfolio", "contact", "connect"],
+	    anchors: ["home", "aboutme", "skills", "portfolio", "contact", "connect"],
+	    menu: "#myMenu",
+	    fitToSection: false,
 
-      //using index
-      if (index == 1) {
-        /* add opacity to arrow */
-        $(".fa-chevron-down").each(function () {
-          $(this).css("opacity", "1");
-        });
-        $(".header-links a").each(function () {
-          $(this).css("color", "white");
-        });
-        $(".header-links").css("background-color", "transparent");
-      } else if (index != 1) {
-        $(".header-links a").each(function () {
-          $(this).css("color", "black");
-        });
-        $(".header-links").css("background-color", "white");
-      }
+	    afterLoad: function (anchorLink, index) {
+	      let loadedSection = $(this);
 
-      //using index
-      if (index == 3) {
-        /* animate skill bars */
-        $(".skillbar").each(function () {
-          $(this)
-            .find(".skillbar-bar")
-            .animate({
-                width: $(this).attr("data-percent")
-              },
-              2500
-            );
-        });
-      }
-    }
-  });
+	      //using index
+	      if (index == 1) {
+	        /* add opacity to arrow */
+	        $(".fa-chevron-down").each(function () {
+	          $(this).css("opacity", "1");
+	        });
+	        $(".header-links a").each(function () {
+	          $(this).css("color", "white");
+	        });
+	        $(".header-links").css("background-color", "transparent");
+	      } else if (index != 1) {
+	        $(".header-links a").each(function () {
+	          $(this).css("color", "black");
+	        });
+	        $(".header-links").css("background-color", "white");
+	      }
 
-  // move section down one
-  $(document).on("click", "#moveDown", function () {
-    $.fn.fullpage.moveSectionDown();
-  });
+	      //using index
+	      if (index == 3) {
+	        /* animate skill bars */
+	        $(".skillbar").each(function () {
+	          $(this)
+	            .find(".skillbar-bar")
+	            .animate({
+	                width: $(this).attr("data-percent")
+	              },
+	              2500
+	            );
+	        });
+	      }
+	    }
+	  });
 
-  // fullpage.js link navigation
-  $(document).on("click", "#aboutme", function () {
-    $.fn.fullpage.moveTo(2);
-  });
+	  // move section down one
+	  $(document).on("click", "#moveDown", function () {
+	    $.fn.fullpage.moveSectionDown();
+	  });
 
-    $(document).on("click", "#skills", function () {
-    $.fn.fullpage.moveTo(3);
-  });
+	  // fullpage.js link navigation
+	  $(document).on("click", "#aboutme", function () {
+	    $.fn.fullpage.moveTo(2);
+	  });
 
-  $(document).on("click", "#projects", function () {
-    $.fn.fullpage.moveTo(4);
-  });
+	  $(document).on("click", "#skills", function () {
+	    $.fn.fullpage.moveTo(3);
+	  });
 
-  $(document).on("click", "#contact", function () {
-    $.fn.fullpage.moveTo(5);
-  });
+	  $(document).on("click", "#projects", function () {
+	    $.fn.fullpage.moveTo(4);
+	  });
 
-  // smooth scrolling
-  $(function () {
-    $("a[href*=#]:not([href=#])").click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        let target = $(this.hash);
-        target = target.length ?
-          target :
-          $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html,body").animate({
-              scrollTop: target.offset().top
-            },
-            700
-          );
-          return false;
-        }
-      }
-    });
-  });
-});
+	  $(document).on("click", "#contact", function () {
+	    $.fn.fullpage.moveTo(5);
+	  });
+
+	  // smooth scrolling
+	  $(function () {
+	    $("a[href*=#]:not([href=#])").click(function () {
+	      if (
+	        location.pathname.replace(/^\//, "") ==
+	        this.pathname.replace(/^\//, "") &&
+	        location.hostname == this.hostname
+	      ) {
+	        let target = $(this.hash);
+	        target = target.length ?
+	          target :
+	          $("[name=" + this.hash.slice(1) + "]");
+	        if (target.length) {
+	          $("html,body").animate({
+	              scrollTop: target.offset().top
+	            },
+	            700
+	          );
+	          return false;
+	        }
+	      }
+	    });
+	  });
+	});
